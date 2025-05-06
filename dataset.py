e34998b6e66c2d75de3ec78590208c408a1045ed
JosephSav <112140162+JosephSav@users.noreply.github.com>
2025-05-06 16:27:18 +1000

    Added Dataset from local storage option

--- dataset.py
+++ dataset.py
@@ -3,11 +3,12 @@ from torchvision import transforms
 from PIL import Image
 from io import BytesIO
 import gcsfs
+import os
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-class SketchDataset(Dataset):
+class SketchDatasetFromCloud(Dataset):
     def __init__(self, gcs_path_prefix, class_names, transform=None, limit_per_class=1000):
         self.fs = gcsfs.GCSFileSystem()
         self.samples = []
@@ -30,18 +31,23 @@ class SketchDataset(Dataset):
             image = self.transform(image)
         return image, label
     
+class SketchDataset(Dataset):
+    def __init__(self, local_path_prefix, class_names, transform=None, limit_per_class=1000):
+        self.samples = []
+        self.transform = transform
 
-# # Example usage
-# class_names = ["cat", "apple", "dog"]
-# transform = transforms.ToTensor()
-# dataset = SketchDataset("quickdraw_dataset/full/numpy_bitmap", class_names, transform=transform)
-# loader = DataLoader(dataset, batch_size=4, shuffle=True)
+        for label, class_name in enumerate(class_names):
+            file_path = os.path.join(local_path_prefix, f"{class_name}.npy")
+            data = np.load(file_path)
+            for img_array in data[:limit_per_class]:
+                self.samples.append((img_array, label))
 
-# for batch_idx, (images, labels) in enumerate(loader):
-#     for i in range(images.size(0)):
-#         plt.imshow(images[i][0], cmap='gray')
-#         plt.title(f"Label: {class_names[labels[i]]}")
-#         plt.show()
+    def __len__(self):
+        return len(self.samples)
     
-#     if batch_idx == 3:  # stop after 2 batches
-#         break
+    def __getitem__(self, idx):
+        img_array, label = self.samples[idx]
+        image = Image.fromarray(img_array.reshape(28, 28).astype(np.uint8), mode='L')
+        if self.transform:
+            image = self.transform(image)
+        return image, label
